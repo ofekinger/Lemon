2797521c238a64745a5344c70222ffc900cc26ab
Ofek Inger <ofekinger@yahoo.com>
2019-08-02 16:13:14 +0300

    Sanity check

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+from telegram.ext import Updater, CommandHandler
+
+updater = Updater(token='905653274:AAHrZ0mPd2LnZ2joBDtHMEnsIbrdueTYRiU')
+
+
+def start(update, context):
+    context.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+
+start_handler = CommandHandler('start', start)
+updater.dispatcher.add_handler(start_handler)
+updater.start_polling()
